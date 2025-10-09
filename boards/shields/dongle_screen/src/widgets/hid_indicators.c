b8fdebffbad796c7b75a2bae0c59acadcb8ef9d7
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-09 15:58:11 -0500

    Update hid_indicators.c

--- boards/shields/dongle_screen/src/widgets/hid_indicators.c
+++ boards/shields/dongle_screen/src/widgets/hid_indicators.c
@@ -86,7 +86,7 @@ int zmk_widget_hid_indicators_init(struct zmk_widget_hid_indicators *widget, lv_
     widget->num_label = lv_label_create(widget->cont);
     lv_obj_align(widget->num_label, LV_ALIGN_LEFT_MID, 50, 0);
     widget->scroll_label = lv_label_create(widget->cont);
-    lv_obj_align(widget->scroll_label, LV_ALIGN_LEFT_MID, 100, 0);
+    lv_obj_align(widget->scroll_label, LV_ALIGN_LEFT_MID, 95, 0);
 
     // Optional: add some spacing between labels
     // lv_obj_set_style_pad_gap(widget->cont, 8, 0);
