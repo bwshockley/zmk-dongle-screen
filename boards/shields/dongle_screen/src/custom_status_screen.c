95e65250f56a7a7a741e327ba955d98241e74470
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-12 14:23:41 -0500

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -63,8 +63,8 @@ lv_obj_t *zmk_display_status_screen()
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
-    zmk_widget_brightness_status_init(&output_brightness_widget, screen);
-    lv_obj_align(zmk_widget_brightness_status_obj(&output_brightness_widget), LV_ALIGN_CENTER, 0, 0);
+    zmk_widget_brightness_status_init(&brightness_status_widget, screen);
+    lv_obj_align(zmk_widget_brightness_status_obj(&brightness_status_widget), LV_ALIGN_CENTER, 0, 0);
     
 #if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
     zmk_widget_output_status_init(&output_status_widget, screen);
