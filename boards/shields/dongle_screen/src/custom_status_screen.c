e72e9dd68012609d706c406d096d61bf41ac9763
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-12 10:25:00 -0500

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -61,7 +61,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 10);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_BATTERY_ACTIVE
@@ -82,7 +82,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_HID_INDICATORS_ACTIVE
     zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
-    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_LEFT, 25, -60);
+    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_LEFT_MID, 20, 30);
 #endif
 /*
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
@@ -93,7 +93,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
     zmk_widget_mod_status_init(&mod_widget, screen);
-    lv_obj_align(zmk_widget_mod_status_obj(&mod_widget), LV_ALIGN_RIGHT_MID, 10, 0);
+    lv_obj_align(zmk_widget_mod_status_obj(&mod_widget), LV_ALIGN_RIGHT_MID, 0, 0);
 #endif
 
     return screen;
