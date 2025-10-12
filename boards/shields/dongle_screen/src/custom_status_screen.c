ce10ad7d64523cce0a508629687f26eeb075b9fc
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-12 12:38:47 -0500

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -61,7 +61,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, -5, 20);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, -25, 15);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_BATTERY_ACTIVE
