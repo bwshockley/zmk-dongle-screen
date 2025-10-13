da63457a7ce97f43eed87a6f1b0cdf31f37117ad
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-13 17:58:33 -0500

    Update brightness_status.c

--- boards/shields/dongle_screen/src/widgets/brightness_status.c
+++ boards/shields/dongle_screen/src/widgets/brightness_status.c
@@ -26,7 +26,7 @@ int zmk_widget_update_brightness_status(struct zmk_widget_brightness_status *wid
     // Setup fade-out animation
     lv_anim_t a;
     lv_anim_init(&a);
-    lv_anim_set_var(&a, widget);
+    lv_anim_set_var(&a, widget->obj);
     lv_anim_set_exec_cb(&a, brightness_status_fade_cb);
     lv_anim_set_values(&a, LV_OPA_60, LV_OPA_TRANSP);
     lv_anim_set_time(&a, BRIGHTNESS_FADE_TIME_MS);
