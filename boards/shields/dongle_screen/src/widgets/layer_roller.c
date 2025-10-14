77111a825854d6a96d46abbebdea808139ca7942
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-14 15:52:12 -0500

    Update layer_roller.c

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -8,7 +8,7 @@
 
 #include <fonts.h>
 
-t#include <zephyr/logging/log.h>
+#include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static char layer_names_buffer[256] = {0}; // Buffer for concatenated layer names
