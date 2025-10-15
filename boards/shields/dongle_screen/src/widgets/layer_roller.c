71c7de5fd68824a1a4e5f9a1396d772ad9577c67
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-14 22:53:59 -0500

    Update layer_roller.c

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -111,7 +111,7 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
 
     // Set the background opacity, text size, and color for the selected layer.
     lv_roller_set_visible_row_count(widget->obj, 3);
-    lv_obj_set_style_text_align(widget->obj, LV_TEXT_LEFT_MID, LV_PART_SELECTED);
+    lv_obj_set_style_text_align(widget->obj, LV_ALIGN_LEFT_MID, LV_PART_SELECTED);
     lv_obj_set_style_bg_opa(widget->obj, LV_OPA_TRANSP, LV_PART_SELECTED);
     lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_48, LV_PART_SELECTED);   
     lv_obj_set_style_text_color(widget->obj, lv_color_hex(0xffffff), LV_PART_SELECTED);
