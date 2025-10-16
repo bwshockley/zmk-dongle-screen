eade313854d645e95e062e73de278dccc6f025bb
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-16 16:57:01 -0500

    Update layer_roller.c

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -114,11 +114,11 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     // Set the background opacity, text size, and color for the selected layer.
     lv_obj_set_style_text_align(widget->obj, LV_ALIGN_LEFT_MID, LV_PART_SELECTED);
     lv_obj_set_style_bg_opa(widget->obj, LV_OPA_TRANSP, LV_PART_SELECTED);
-    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_36, LV_PART_SELECTED);   
+    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_48, LV_PART_SELECTED);   
     lv_obj_set_style_text_color(widget->obj, lv_color_hex(0xffffff), LV_PART_SELECTED);
 
     // Set the text size and color of the non-selected layers.
-    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_32, LV_PART_MAIN);
+    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_40, LV_PART_MAIN);
     lv_obj_set_style_text_color(widget->obj, lv_color_hex(0x050505), LV_PART_MAIN);
 
     layer_names_buffer[0] = '\0';
