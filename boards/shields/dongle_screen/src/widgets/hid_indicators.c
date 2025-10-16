2df5e134e0513d8f01f8d6333ca9f8e2906680ff
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-16 16:27:37 -0500

    Update hid_indicators.c

--- boards/shields/dongle_screen/src/widgets/hid_indicators.c
+++ boards/shields/dongle_screen/src/widgets/hid_indicators.c
@@ -48,9 +48,9 @@ static void set_hid_indicators(struct zmk_widget_hid_indicators *widget, struct 
     lv_color_t scroll_color = scroll ? active_color : inactive_color;
 
     // Set the icon based on locked or unlocked.
-    cap_icon_choice = caps ? LOCK : UNLOCK;
-    num_icon_choice = num ? LOCK : UNLOCK;
-    scr_icon_choice = scroll ? LOCK : UNLOCK;
+    char[12] cap_icon_choice = caps ? LOCK : UNLOCK;
+    char[12] num_icon_choice = num ? LOCK : UNLOCK;
+    char[12] scr_icon_choice = scroll ? LOCK : UNLOCK;
 
     // Set label colors and text
     lv_obj_set_style_text_font(widget->caps_icon, &icons_lvgl, 0);
