7bd267fb37621cfbca4037e25b801c2e9000d44f
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-16 14:18:56 -0500

    Update icons_lvgl.c

--- boards/shields/dongle_screen/src/fonts/icons_lvgl.c
+++ boards/shields/dongle_screen/src/fonts/icons_lvgl.c
@@ -351,7 +351,6 @@ lv_font_t icons_lvgl = {
     .underline_position = -1,
     .underline_thickness = 1,
 #endif
-    .static_bitmap = 0,
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
     .fallback = NULL,
