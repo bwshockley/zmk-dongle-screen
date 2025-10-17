5d8ee884537c61558d08da736988c5f829a71add
Benjamin Shockley <benjaminshockley@hotmail.com>
2025-10-17 00:15:49 -0500

    Update layer_roller.c

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -13,10 +13,9 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static char layer_names_buffer[256] = {0}; // Buffer for concatenated layer names
 
-static int layer_select_id[6] = {2, 4, 3, 1, 0, 5}; // Select order of layers.
+static int layer_select_id[6] = {2, 4, 3, 1, 0}; // Select order of layers.
 
-//static int layer_display_order[6] = {5, 1, 2, 0, 3, 4}; // Display order of layers.
-static int layer_display_order[6] = {4, 3, 0, 2, 1, 5}; // Display order of layers.
+static int layer_display_order[6] = {4, 3, 0, 2, 1}; // Display order of layers.
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
